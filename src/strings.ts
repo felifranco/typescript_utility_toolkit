b3d1be877e6044fb27f15dc71f42c41c4bf39c3a
Feliciano Franco <f64franco@gmail.com>
2025-01-01 22:17:01 -0600

    feat: Added TypeDoc npm package

--- src/strings.ts
+++ src/strings.ts
@@ -1,7 +1,7 @@
 /**
  * Acorta una cadena a una longitud específica y añade "…" si es necesario.
- * @param text Cadena de texto que se desea acortar
- * @param maxLength Número máximo de caracteres que se desea mostrar
+ * @param text - Cadena de texto que se desea acortar
+ * @param maxLength - Número máximo de caracteres que se desea mostrar
  * @returns Cadena de texto acortada
  */
 export const truncate = (text: string, maxLength: number): string => {
